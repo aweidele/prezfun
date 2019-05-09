694e6cdd40458037c3bbf8bb224212e9a9ca24b0
Aaron Weidele <aweidele@r2integrated.com>
2019-05-09 16:46:48 -0400

    crunch the data

--- index.php
+++ index.php
@@ -1,190 +1,190 @@
 <?php
-  $pres = date('Y');
+  $pres = intval(date('Y')) + 1;
   $prez = [
     [
       "name" => "Washington",
       "start" => 1789,
-      "died" => $pres
+      "died" => 1799
     ],
     [
       "name" => "J Adams",
       "start" => 1797,
-      "died" => $pres
+      "died" => 1826
     ],
     [
       "name" => "Jefferson",
       "start" => 1801,
-      "died" => $pres
+      "died" => 1826
     ],
     [
       "name" => "Madison",
       "start" => 1809,
-      "died" => $pres
+      "died" => 1836
     ],
     [
       "name" => "Monroe",
       "start" => 1817,
-      "died" => $pres
+      "died" => 1831
     ],
     [
       "name" => "JQ Adams",
       "start" => 1825,
-      "died" => $pres
+      "died" => 1848
     ],
     [
       "name" => "Jackson",
       "start" => 1829,
-      "died" => $pres
+      "died" => 1845
     ],
     [
       "name" => "Van Buren",
       "start" => 1837,
-      "died" => $pres
+      "died" => 1862
     ],
     [
       "name" => "WH Harrison",
       "start" => 1841,
-      "died" => $pres
+      "died" => 1841
     ],
     [
       "name" => "Tyler",
       "start" => 1841,
-      "died" => $pres
+      "died" => 1862
     ],
     [
       "name" => "Polk",
       "start" => 1845,
-      "died" => $pres
+      "died" => 1849
     ],
     [
       "name" => "Taylor",
       "start" => 1849,
-      "died" => $pres
+      "died" => 1850
     ],
     [
       "name" => "Fillmore",
       "start" => 1850,
-      "died" => $pres
+      "died" => 1874
     ],
     [
       "name" => "Pierce",
       "start" => 1853,
-      "died" => $pres
+      "died" => 1869
     ],
     [
       "name" => "Buchanan",
       "start" => 1857,
-      "died" => $pres
+      "died" => 1868
     ],
     [
       "name" => "Lincoln",
       "start" => 1861,
-      "died" => $pres
+      "died" => 1865
     ],
     [
       "name" => "A Johnson",
       "start" => 1865,
-      "died" => $pres
+      "died" => 1875
     ],
     [
       "name" => "Grant",
       "start" => 1869,
-      "died" => $pres
+      "died" => 1885
     ],
     [
       "name" => "Hayes",
       "start" => 1877,
-      "died" => $pres
+      "died" => 1893
     ],
     [
       "name" => "Garfield",
       "start" => 1881,
-      "died" => $pres
+      "died" => 1881
     ],
     [
       "name" => "Arthur",
       "start" => 1881,
-      "died" => $pres
+      "died" => 1886
     ],
     [
       "name" => "Cleveland",
       "start" => 1885,
-      "died" => $pres
+      "died" => 1908
     ],
     [
       "name" => "B Harrison",
       "start" => 1889,
-      "died" => $pres
+      "died" => 1901
     ],
     [
       "name" => "McKinley",
       "start" => 1897,
-      "died" => $pres
+      "died" => 1901
     ],
     [
       "name" => "T Roosevelt",
       "start" => 1901,
-      "died" => $pres
+      "died" => 1919
     ],
     [
       "name" => "Taft",
       "start" => 1909,
-      "died" => $pres
+      "died" => 1930
     ],
     [
       "name" => "Wilson",
       "start" => 1913,
-      "died" => $pres
+      "died" => 1924
     ],
     [
       "name" => "Harding",
       "start" => 1921,
-      "died" => $pres
+      "died" => 1923
     ],
     [
       "name" => "Coolidge",
       "start" => 1923,
-      "died" => $pres
+      "died" => 1933
     ],
     [
       "name" => "Hoover",
       "start" => 1929,
-      "died" => $pres
+      "died" => 1964
     ],
     [
       "name" => "FD Roosevelt",
       "start" => 1933,
-      "died" => $pres
+      "died" => 1945
     ],
     [
       "name" => "Truman",
       "start" => 1945,
-      "died" => $pres
+      "died" => 1972
     ],
     [
       "name" => "Eisenhower",
       "start" => 1953,
-      "died" => $pres
+      "died" => 1969
     ],
     [
       "name" => "Kennedy",
       "start" => 1961,
-      "died" => $pres
+      "died" => 1963
     ],
     [
       "name" => "LB Johnson",
       "start" => 1963,
-      "died" => $pres
+      "died" => 1973
     ],
     [
       "name" => "Nixon",
       "start" => 1969,
-      "died" => $pres
+      "died" => 1994
     ],
     [
       "name" => "Ford",
       "start" => 1974,
-      "died" => $pres
+      "died" => 2006
     ],
     [
       "name" => "Carter",
@@ -194,12 +194,12 @@
     [
       "name" => "Reagan",
       "start" => 1981,
-      "died" => $pres
+      "died" => 2004
     ],
     [
       "name" => "GHW Bush",
       "start" => 1989,
-      "died" => $pres
+      "died" => 2018
     ],
     [
       "name" => "Clinton",
@@ -222,6 +222,29 @@
       "died" => $pres
     ]
   ];
+
+  $years = [];
+  $sort = [];
+  for ($i = 1789; $i < $pres; $i++) {
+    $a = 0;
+    $list = [];
+    $d = [];
+    foreach($prez as $key => $p) {
+      if($p["start"] <= $i && $p["died"] >= $i) {
+      //if($p["start"] >= $i && $p["died"] <= $i) {
+        $a++;
+        $list[] = $p["name"];
+        if($p["died"] == $i) {
+          $d[] = $p["name"];
+        }
+      }
+    }
+    $years[$i] = [
+      "alive" => $a,
+      "prez" => $list,
+      "died" => $d
+    ];
+  }
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -234,11 +257,12 @@
 </head>
 <body>
   <table>
-    <?php foreach($prez as $p) { ?>
+    <?php foreach($years as $key => $year) { ?>
       <tr>
-        <td><?= $p["name"] ?></td>
-        <td><?= $p["start"] ?></td>
-        <td><?= $p["died"] ?></td>
+        <td><?= $key ?></td>
+        <td><?= $year["alive"] ?></td>
+        <td><?= implode(", ",$year["prez"]) ?></td>
+        <td><?= implode(", ",$year["died"]) ?></td>
       </tr>
     <?php } ?>
   </table>
